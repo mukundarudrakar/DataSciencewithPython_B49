0406e44b1739e89372bac1f12f1ca7d75cae777e
mukundarudrakar <139313932+mukundarudrakar@users.noreply.github.com>
2023-07-12 01:16:35 -0400

    Add files via upload

---
+++ Mukunda.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 20
+#a is a variable
+#= is a assignment operator
+#20 is data
+print(a)
+
+
+# In[2]:
+
+
+a
+
+
+# In[10]:
+
+
+a = "Mukunda"
+b = "Rudrakar"
+#b is a variable
+
+
+# In[15]:
+
+
+a+' '+b
+
+
+# In[17]:
+
+
+print("Mukunda\tRudrakar")
+
+
+# In[18]:
+
+
+print("Mukunda\nRudrakar")
+
+
+# In[19]:
+
+
+a = 20
+type(a)
+
+
+# In[20]:
+
+
+a = "Mukunda"
+type(a)
+
+
+# In[ ]:
+
+
+
+
